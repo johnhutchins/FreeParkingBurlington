e30e25c073b587c6847939f089fd80b3c67a9ee7
John Hutchins <john@aseba.org>
2018-02-28 15:05:59 -0500

    Intial + set up maps

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  constructor() { }
+
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(44.4759, 73.2121),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+
+}
+
